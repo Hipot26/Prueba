64ca0f44c6f2cb09241709e94183930974a883c7
hipot <26.jotate.26@gmail.com>
2023-12-19 11:10:36 -0500

    primer estado

---
+++ Baken.php
@@ -0,0 +1,102 @@
+<?php
+class UsuariosManager {
+    private $conn;
+
+    public function __construct($servername, $username, $password, $database) {
+        $this->conn = new mysqli($servername, $username, $password, $database);
+
+        if ($this->conn->connect_error) {
+            die("Conexión fallida: " . $this->conn->connect_error);
+        }
+    }
+
+    public function agregarUsuario($id_usuario, $nombre, $email, $telefono) {
+        $sql = "INSERT INTO usuarios (id_usuario, nombre, email, telefono) VALUES (?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("isss", $id_usuario, $nombre, $email, $telefono);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function eliminarUsuario($id_usuario) {
+        $sql = "DELETE FROM usuarios WHERE id_usuario = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $id_usuario);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function listarUsuarios() {
+        $sql = "SELECT id_usuario, nombre, email, telefono FROM usuarios";
+        $result = $this->conn->query($sql);
+
+        $usuarios = [];
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $usuarios[] = $row;
+            }
+        }
+
+        return $usuarios;
+    }
+
+    public function actualizarUsuario($id_usuario, $nombre, $email, $telefono) {
+        $sql = "UPDATE usuarios SET nombre=?, email=?, telefono=? WHERE id_usuario=?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("sssi", $nombre, $email, $telefono, $id_usuario);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function cerrarConexion() {
+        $this->conn->close();
+    }
+}
+
+// Uso de la clase UsuariosManager
+$servername = '127.0.0.1';
+$username = 'root';
+$password = '0215487';
+$database = "prueba";
+
+$manager = new UsuariosManager($servername, $username, $password, $database);
+
+// Manejo de solicitudes desde el frontend
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if (isset($data["accion"]) && $data["accion"] === "agregar") {
+        $id_usuario = $data["id_usuario"];
+        $nombre = $data["nombre"];
+        $email = $data["email"];
+        $telefono = $data["telefono"];
+
+        if ($manager->agregarUsuario($id_usuario, $nombre, $email, $telefono)) {
+            echo "Usuario agregado correctamente.";
+        } else {
+            echo "Error al agregar usuario.";
+        }
+    } else {
+        echo "Acción no válida para la solicitud POST.";
+    }
+} elseif ($_SERVER["REQUEST_METHOD"] === "GET") {
+    // Listar usuarios y devolver los datos en formato JSON
+    header('Content-Type: application/json');
+    echo json_encode($manager->listarUsuarios());
+}
+
+$manager->cerrarConexion(); // Cerrar la conexión
+?>
---
+++ Index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Usuarios</title>
+    <!-- Agrega el CDN de Vue.js -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js"></script>
+</head>
+<body>
+    <div id="app">
+        <h1>Lista de Usuarios</h1>
+        <button @click="obtenerUsuarios">Listar Usuarios</button>
+        <button @click="mostrarFormulario">Agregar Usuario</button>
+
+        <div v-if="mostrarAgregar">
+            <h2>Agregar Nuevo Usuario</h2>
+            <form @submit.prevent="agregarNuevoUsuario">
+                <label for="id_usuario">Nu. Identificacion:</label>
+                <input type="number" id="id_usuario" v-model="nuevoUsuario.id_usuario" required><br><br>    
+
+                <label for="nombre">Nombre:</label>
+                <input type="text" id="nombre" v-model="nuevoUsuario.nombre" required><br><br>
+        
+                <label for="email">Email:</label>
+                <input type="email" id="email" v-model="nuevoUsuario.email" required><br><br>
+        
+                <label for="telefono">Telefono:</label>
+                <input type="number" id="telefono" v-model.number="nuevoUsuario.telefono" required><br><br>
+        
+                <button type="submit">Agregar</button>
+            </form>
+        </div>
+
+        <button @click="eliminarUsuario">Eliminar Usuario</button>
+        <button @click="actualizarUsuario">Actualizar Usuario</button>
+
+        <table v-if="usuarios.length > 0" border="1">
+            <thead>
+                <tr>
+                    <th>Identificacion</th>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>Telefono</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="usuario in usuarios" :key="usuario.id_usuario">
+                    <td>{{ usuario.id_usuario }}</td>
+                    <td>{{ usuario.nombre }}</td>
+                    <td>{{ usuario.email }}</td>
+                    <td>{{ usuario.telefono }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <p v-else>No hay usuarios cargados</p>
+    </div>
+
+    <script>
+        new Vue({
+            el: '#app',
+            data: {
+                usuarios: [],
+                mostrarAgregar: false,
+                nuevoUsuario: {
+                    id_usuario:'',
+                    nombre: '',
+                    email: '',
+                    telefono: ''
+                }
+            },
+            methods: {
+                obtenerUsuarios() {
+                    fetch('Baken.php') // Ruta del php
+                        .then(response => response.json())
+                        .then(data => {
+                            this.usuarios = data;
+                        })
+                        .catch(error => console.error('Error:', error));
+                },
+                mostrarFormulario() {
+                    this.mostrarAgregar = !this.mostrarAgregar;
+                },
+                agregarNuevoUsuario() {
+                    fetch('Baken.php', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            accion: 'agregar',
+                            id_usuario: this.nuevoUsuario.id_usuario,
+                            nombre: this.nuevoUsuario.nombre,
+                            email: this.nuevoUsuario.email,
+                            telefono: this.nuevoUsuario.telefono
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(response => response.text())
+                    .then(data => {
+                        console.log(data); // Manejo de la respuesta del servidor
+                        this.obtenerUsuarios(); // Actualiza la lista después de agregar
+                    })
+                    .catch(error => console.error('Error:', error));
+                },
+                eliminarUsuario() {
+                    // Lógica para eliminar usuario, similar al método agregarNuevoUsuario
+                },
+                actualizarUsuario() {
+                    // Lógica para actualizar usuario, similar al método agregarNuevoUsuario
+                }
+            }
+        });
+    </script>
+</body>
+</html>
